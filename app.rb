06990cc31ceb84d065318f93854d6061bbba4470
Jo Zhouzheng <jo@Jos-MacBook.local>
2018-09-23 19:30:09 -0400

    new edits

--- app.rb
+++ app.rb
@@ -25,72 +25,120 @@ get "/" do
 end
 
 
-empty_array = []
-greetings = ["Howdy!", "Hola.", "Hiyaya.", "I'm sun-bathing, what are you doing?", "Heya.", "Hey you.","Moin"]
+# empty_array = []
+# greetings = ["Howdy!", "Hola.", "Hiyaya.", "I'm sun-bathing, what are you doing?", "Heya.", "Hey you.","Moin"]
+#
+# get '/about' do
+# 	session["visits"] ||= 0 # Set the session to 0 if it hasn't been set before
+#   session["visits"] = session["visits"] + 1
+#
+#   time = Time.new
+#   num_choice = rand(7) #To random greetings
+#
+# 	if !session[:first_name].nil? && !session[:number].nil?
+# 	     greetings.sample + ", " + "#{session[:first_name]} (#{session[:number]})
+# 	     <br />
+# 	     Total visits: #{session[:visits].to_s} times as #{time.strftime("%A %B %d, %Y %H:%M")}"
+# 	else
+# 	     "Bear is a fun-loving, caring and insightful bear. Try talk to him and see what he says :)
+# 	     <br />
+# 	     You have visited #{session[:visits].to_s} times as of #{time.strftime("%A %B %d, %Y %H:%M")}"#returns mmddyy
+# 	end
+# end
 
-get '/about' do
-	session["visits"] ||= 0 # Set the session to 0 if it hasn't been set before
-  session["visits"] = session["visits"] + 1
+#
+# get '/signup' do
+#   	"Sign up for Bear" + "</br>" + "Enter your name"
+# end
+#
+# get '/signup/:first_name/:number' do
+#   	session[:first_name] = params[:first_name]
+#   	session[:number] = params[:number]
+#   	"<h1> Hi, </h1>" + params[:first_name] + ", "+ params[:number]
+# end
+#
+#   post "/signup" do
+#     if params[:first_name] == "" || params[:number]==""
+#       return "Your information in incomplete."
+#     else
+#       client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
+#
+#       # Include a message here
+#       message = "Hi" + params[:first_name] + ", welcome to Bear! I can respond to who, what, where, when and why. If you're stuck, type help."
+#       # this will send a message from any end point
+#       client.api.account.messages.create(
+#         from: ENV["TWILIO_FROM"],
+#         to: params[:number],
+#         body: message
+#       )
+#   	   # response if eveything is OK
+#   	   return "You're signed up. You'll receive a text message in a few minutes from Bear. "
+#      end
+#    end
 
-  time = Time.new
-  num_choice = rand(7) #To random greetings
 
-	if !session[:first_name].nil? && !session[:number].nil?
-	     greetings.sample + ", " + "#{session[:first_name]} (#{session[:number]})
-	     <br />
-	     Total visits: #{session[:visits].to_s} times as #{time.strftime("%A %B %d, %Y %H:%M")}"
-	else
-	     "Bear is a fun-loving, caring and insightful bear. Try talk to him and see what he says :)
-	     <br />
-	     You have visited #{session[:visits].to_s} times as of #{time.strftime("%A %B %d, %Y %H:%M")}"#returns mmddyy
-	end
-end
+get "/incoming/sms" do
+  session["last_intent"] ||= nil
 
-
-get '/signup' do
-  	"Sign up for Bear" + "</br>" + "Enter your name"
-end
-
-get '/signup/:first_name/:number' do
-  	session[:first_name] = params[:first_name]
-  	session[:number] = params[:number]
-  	"<h1> Hi, </h1>" + params[:first_name] + ", "+ params[:number]
-end
-
-  post "/signup" do
-    if params[:first_name] == "" || params[:number]==""
-      return "Your information in incomplete."
-    else
-      client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
-
-      # Include a message here
-      message = "Hi" + params[:first_name] + ", welcome to Bear! I can respond to who, what, where, when and why. If you're stuck, type help."
-      # this will send a message from any end point
-      client.api.account.messages.create(
-        from: ENV["TWILIO_FROM"],
-        to: params[:number],
-        body: message
-      )
-  	   # response if eveything is OK
-  	   return "You're signed up. You'll receive a text message in a few minutes from Bear. "
-     end
-   end
-
-get '/incoming/sms' do
   session["counter"] ||= 1
-  body = params[:Body] || ""
+  count = session["counter"]
+
   sender = params[:From] || ""
+  body = params[:Body] || ""
+  body = body.downcase.strip
 
   if session["counter"] == 1
-    message = "Welcome to Bear! I'm a fun-loving, caring and insightful bear. Try talk to me and see what I say 😉 "
+    message = "It's Bear here 🐻. I'm a fun-loving, caring and insightful bear. Try talk to me and see what I say 😉 . Type in 'questions' to get a list of questions you can ask me."
     media = "https://media.giphy.com/media/xNQTp4xqjY22I/giphy.gif"
-  else
-    message = "It's Bear here 🐻. What's up? "#How do i add first name
-    media = "https://media.giphy.com/media/lII8kUqoT5MD6/giphy.gif"
-    #media = nil
-  end
+  elsif body.include? "like to do" or body.include? "like doing" #when user ask what do you like to do
+    message = "I like to eat honey, and read! I'm a well-read bear. Give me a book title and I will let you know what I think"
+      # media = #gif
+  elsif body.include? "quote" #"what's your favorite quote?"
+    message = "A well-read bear is a dangerous creature.🤓"
+  elsif body.include? "questions"
+    message = "You can ask me questions such as:
 
-  #try ask what do you do everyday, selecting among giphy" "this is what i like to do" 'want to see more?'
+    \"What do you like to do?\"
+
+    \"What's your favorite quote?\"
+
+    \"What books are you currently reading?\"
+
+    \"What do you like the most?\"
+
+    \"What books are you currently reading?\"
+
+    \"What are Bear's favorite books?\""
+  elsif body == "What are Bear's favorite books?"
+    message = "You just picked my favorite question! Hummm but I can only share it if you feed me honey 🍯😎...alright alrigh here you go:
+
+    \"Man's Search for Meaning\"
+
+    \"Sapiens: A Brief History of Humankind\"
+
+    \"Walden\"
+
+    \"The Unbearable Lightness of Being\"
+
+    \"Quiet\"
+
+    \"The Wisdom of Life\"
+
+    Try type in the title of the book and I will let you know what I think about the book!"  
+    media ="https://media.giphy.com/media/126BrhLh4YgwkE/giphy.gif"
+  elsif body == "What do you like the most?"
+    message = "Food...and honey! "
+    media = "https://media.giphy.com/media/fdWVI1op6wi88/giphy.gif"
+  elsif body == "What do you like to do?"
+    message = "Cuddle with my pillow."
+    media = "https://media.giphy.com/media/2QIbGQ1WEVF6M/giphy.gif"
+
+  else
+    message = "I didn't understand that. You can type in 'questions' to get a list of questions you can ask me."
+end
+#
+
+  #try ask what do you do everyday, selecting among giphy" "this is what i like to do" "want to see more?"
   #'tell me a little bit more about yourself, and i will tell you more'
 
 # Build a twilio response object
@@ -115,54 +163,3 @@ get '/incoming/sms' do
   content_type 'text/xml'
   twiml.to_s
  end
-
-get "/test/sms" do
-
-  client = Twilio::REST::Client.new ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"]
-
-   # Include a message here
-  message = "Hi! This is a test SMS from your Sinatra application."
-
-   # this will send a message from any end point
-  client.api.account.messages.create(
-     from: ENV["TWILIO_FROM"],
-     to: ENV["TEST_NUMBER"],
-     body: message
-  )
- 	# response if eveything is OK
- 	"An SMS was sent to your number "
-end
-
-# get '/test/conversation' do
-# 	if params[:Body].nil? && params[:From].nil?
-#  		return "What is your phone number?"
-#  	elsif params[:From].nil?
-#  		return "Please send me your number"
-#  	elsif params[:Body].nil?
-#  		return "Please give me your number"
-#  	end
-#  	  determine_response params[:Body]
-#  end
-
-
- # def determine_response body
- #   laugh = ["haha", "lol", "that's funny!", "omg!!!"]
- #   body = body.downcase.strip
- #
- # 	if body == "hi"
- # 		"I am Bear. I can tell you more about JoJo if we become friends"
- # 	elsif body == "who"
- # 		"I am Bear who can help you know more about Jo. You can reply 'fact' to learn more about her"
- # 	elsif body == "where"
- # 		"I currently in Pittsburgh."
-	# elsif body == "why"
-	# 	"Jo became interested in conversational UI and I am her first app!"
-	# elsif body == "when"
- # 		"I was created in September 2018 in Pittsburgh"
- # 	elsif body == "joke"
- # 		array_of_lines = IO.readlines("jokes.txt")
- # 		return array_of_lines.sample + laugh.sample
- # 	elsif body == "fact"
- # 		array_of_lines = IO.readlines("facts.txt")
- # 		return array_of_lines.sample
- # 	end
